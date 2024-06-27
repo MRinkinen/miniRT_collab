8501f7604534f7c93dd7667de48ec5784b7f46f2
mrinkinen <mattirinkinen@gmail.com>
2024-06-27 15:21:19 +0300

    Camera started

--- includes/minirt.h
+++ includes/minirt.h
@@ -1,46 +1,50 @@
 #ifndef MINIRT_H
-# define MINIRT_H
-
+#define MINIRT_H
 
 #include <stdbool.h>
 #include <stdio.h> // DELETE BEFORE RETURN!!
-# include <stdlib.h>
-# include <fcntl.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <math.h>
-# include "../libft/libft.h"
-# include "../MLX42/include/MLX42/MLX42.h"
+#include "../libft/libft.h"
+#include "../MLX42/include/MLX42/MLX42.h"
 
-# define SCREEN_HEIGHT 1000
-# define SCREEN_WIDHT 1000
+#define SCREEN_HEIGHT 1000
+#define SCREEN_WIDHT 1000
 
-typedef struct s_vec{
-    double e[3];
+typedef struct s_vec
+{
+	double e[3];
 } t_vec3;
 
 typedef struct s_ray
 {
-    t_vec3 orig;
-    t_vec3 dir;
+	t_vec3 orig;
+	t_vec3 dir;
 } t_ray;
 
 typedef struct s_var
 {
-	mlx_t			*mlx;
-	t_vec3			*vector;
-	//double 			e[3];
-	// char			**map;
-	// int				mapcolumns;
-	// int				maprows;
-	// int				text_fd;
-	// int				w;
-	// int				h;
-	// int				collectables;
-	// int				steps;
-	// int				playerstartpos[2];
-	// int				validcheck;
-	// int				enemycounter;
-	// int				opengate;
-	mlx_image_t		    *testimage;
+	mlx_t *mlx;
+	t_vec3 *vector;
+
+	double aspect_ratio;
+	int image_width;
+	int image_height;
+
+	// CAMERA
+	double focal_length;
+	double viewport_height;
+	double viewport_width;
+	t_vec3 camera_center;
+
+	t_vec3 viewport_u;
+	t_vec3 viewport_v;
+
+	// auto pixel_delta_u = viewport_u / image_width;
+	// auto pixel_delta_v = viewport_v / image_height;
+
+	mlx_image_t *testimage;
 	// mlx_image_t		*exit;
 	// mlx_image_t		*wall;
 	// mlx_image_t		*ground;
@@ -53,13 +57,13 @@ typedef struct s_var
 	// mlx_texture_t	*wall_tex;
 	// mlx_texture_t	*ground_tex;
 	// mlx_texture_t	*collect_tex;
-}	t_var;
+} t_var;
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a);
 void write_color(t_vec3 vec, t_var *var, int x, int y);
 
 /*Vector 3*/
 t_vec3 t_vec3_create(double e0, double e1, double e2);
-t_vec3 t_vec3_multiply_scalar(const t_vec3* v, double t);
-t_vec3 t_vec3_add_vectors(const t_vec3* u, const t_vec3* v);
+t_vec3 t_vec3_multiply_scalar(const t_vec3 *v, double t);
+t_vec3 t_vec3_add_vectors(const t_vec3 *u, const t_vec3 *v);
 #endif
--- sources/minirt.c
+++ sources/minirt.c
@@ -2,27 +2,27 @@
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-void printsquare(void* param)
+void printsquare(void *param)
 {
-	t_var	*var;
+	t_var *var;
 	var = param;
-	(void) var;
-	for (int j = 0; j < SCREEN_HEIGHT; j++) 
+	(void)var;
+	for (int j = 0; j < SCREEN_HEIGHT; j++)
 	{
-        for (int i = 0; i < SCREEN_WIDHT; i++) 
+		for (int i = 0; i < SCREEN_WIDHT; i++)
 		{
-			t_vec3 pixel_color = t_vec3_create((double)i/(SCREEN_WIDHT-1), (double)j/(SCREEN_HEIGHT-1), 0);
-            write_color(pixel_color, var,j,i);
-        }
-    }
+			t_vec3 pixel_color = t_vec3_create((double)i / (SCREEN_WIDHT - 1), (double)j / (SCREEN_HEIGHT - 1), 0);
+			write_color(pixel_color, var, j, i);
+		}
+	}
 }
 
-void ft_hook(void* param)
+void ft_hook(void *param)
 {
-	t_var	*var;
+	t_var *var;
 
 	var = param;
 
@@ -32,8 +32,7 @@ void ft_hook(void* param)
 
 // -----------------------------------------------------------------------------
 
-
-void	hooks(t_var *var)
+void hooks(t_var *var)
 {
 	mlx_loop_hook(var->mlx, printsquare, var);
 	mlx_loop_hook(var->mlx, ft_hook, var);
@@ -42,27 +41,38 @@ void	hooks(t_var *var)
 int main(void)
 {
 	t_var var;
-	
+
+	var.aspect_ratio = 16.0 / 9.0;
+	var.image_width = 400;
+
+	var.image_height = var.image_width / (int)var.aspect_ratio;
+	// image_height = (image_height < 1) ? 1 : image_height;
+	var.focal_length = 1.0;
+	var.viewport_height = 2.0;
+	var.viewport_width = var.viewport_height * (double)var.image_width / (double)var.image_height;
+	var.camera_center = t_vec3_create(0, 0, 0);
+	var.viewport_u = t_vec3_create(var.viewport_width, 0, 0);
+	var.viewport_v = t_vec3_create(0, -var.viewport_height, 0);
+
 	if (!(var.mlx = mlx_init(SCREEN_WIDHT, SCREEN_HEIGHT, "MiniRT", false)))
 	{
 		ft_printf("%s", mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	if (!(var.testimage = mlx_new_image(var.mlx, SCREEN_WIDHT, SCREEN_HEIGHT)))
 	{
 		mlx_close_window(var.mlx);
 		ft_printf("%s", mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 
 	if (mlx_image_to_window(var.mlx, var.testimage, 0, 0) == -1)
 	{
 		mlx_close_window(var.mlx);
 		ft_printf("%s", mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	hooks(&var);
-	
 
 	mlx_loop(var.mlx);
 	mlx_terminate(var.mlx);

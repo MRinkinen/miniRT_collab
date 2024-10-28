5e30dc00eef2ef9ff4c3f2ddf063017be0af8946
tvalimak <tvalimak@student.hive.fi>
2024-10-28 14:07:00 +0200

    threads tweaking

--- sources/mlxfunctions_two.c
+++ sources/mlxfunctions_two.c
@@ -6,7 +6,7 @@
 /*   By: tvalimak <tvalimak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 18:47:03 by tvalimak          #+#    #+#             */
-/*   Updated: 2024/10/26 18:47:42 by tvalimak         ###   ########.fr       */
+/*   Updated: 2024/10/28 12:16:23 by tvalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ uint32_t	ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a)
 {
 	return (r << 24 | g << 16 | b << 8 | a);
 }
-
+/*
 void	write_color(t_color col, t_var *var, int x, int y)
 {
 	int	color;
 
 	color = ft_pixel(col.r, col.g, col.b, 255);
 	mlx_put_pixel(var->screenimage, x, y, color);
-}
+}*/
--- sources/render.c
+++ sources/render.c
@@ -6,11 +6,31 @@
 /*   By: tvalimak <tvalimak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 17:07:42 by tvalimak          #+#    #+#             */
-/*   Updated: 2024/10/26 21:24:19 by tvalimak         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:05:21 by tvalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
+#include <pthread.h>
+
+pthread_mutex_t g_write_color_mutex;
+
+typedef struct s_thread_data
+{
+    t_var *var;
+    int pixel_x;
+    int pixel_y;
+} t_thread_data;
+
+void	write_color(t_color col, t_var *var, int x, int y)
+{
+	int	color;
+
+	pthread_mutex_lock(&g_write_color_mutex);
+	color = ft_pixel(col.r, col.g, col.b, 255);
+	mlx_put_pixel(var->screenimage, x, y, color);
+    pthread_mutex_unlock(&g_write_color_mutex);
+}
 
 t_color	shade_pixel(t_var *var, t_ray *r, \
 t_object *closest_object, float closest_t)
@@ -58,6 +78,93 @@ void	process_pixel(t_var *var, int x, int y)
 	write_color(pixel_color, var, x, y);
 }
 
+void *process_pixel_thread(void *arg)
+{
+    t_thread_data *data = (t_thread_data *)arg;
+    process_pixel(data->var, data->pixel_x, data->pixel_y);
+    return NULL;
+}
+
+void printimage(void *param)
+{
+    t_var *var = (t_var *)param;
+    int num_threads = 8;
+    pthread_t threads[num_threads];
+    t_thread_data thread_data[num_threads];
+    int i = 0;
+    int y = 0;
+    int x = 0;
+
+    pthread_mutex_init(&g_write_color_mutex, NULL);
+    while (i < num_threads && y < HEIGHT)
+    {
+        while (x < WIDTH)
+        {
+            thread_data[i].var = var;
+            thread_data[i].pixel_x = x;
+            thread_data[i].pixel_y = y;
+            pthread_create(&threads[i], NULL, process_pixel_thread, &thread_data[i]);
+            i++;
+            x++;
+            if (i == num_threads)
+            {
+                int j = 0;
+                while (j < num_threads)
+                {
+                    pthread_join(threads[j], NULL);
+                    j++;
+                }
+                i = 0;
+            }
+        }
+        x = 0;
+		y++;
+    }
+    int j = 0;
+    while (j < i)
+    {
+        pthread_join(threads[j], NULL);
+        j++;
+    }
+    pthread_mutex_destroy(&g_write_color_mutex);
+}
+
+/*
+void printimage(void *param)
+{
+    t_var *var = (t_var *)param;
+    int num_threads = 8; // Number of threads to use (adjust based on your system)
+    pthread_t threads[num_threads];
+    t_thread_data thread_data[num_threads];
+    int rows_per_thread = HEIGHT / num_threads;
+    int i;
+
+    // Initialize the mutex
+    pthread_mutex_init(&g_write_color_mutex, NULL);
+
+    // Create threads for rendering
+    i = 0;
+    while (i < num_threads)
+    {
+        thread_data[i].var = var;
+        thread_data[i].start_y = i * rows_per_thread;
+        thread_data[i].end_y = (i == num_threads - 1) ? HEIGHT : (i + 1) * rows_per_thread;
+        pthread_create(&threads[i], NULL, render_section, &thread_data[i]);
+        ++i;
+    }
+
+    // Wait for all threads to complete
+    i = 0;
+    while (i < num_threads)
+    {
+        pthread_join(threads[i], NULL);
+        ++i;
+    }
+
+    // Destroy the mutex
+    pthread_mutex_destroy(&g_write_color_mutex);
+}
+
 void	printimage(void *param)
 {
 	t_var	*var;
@@ -74,4 +181,4 @@ void	printimage(void *param)
 			process_pixel(var, x, y);
 		}
 	}
-}
+}*/
